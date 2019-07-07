5642970f71517075bf811c4af97bef7fcd8283cb
Maxime Margaine <margai_m@etna-alternance.net>
2019-07-07 15:14:03 +0200

    Ajout des formulaires des templates

--- resources/views/template/formUpload.blade.php
+++ resources/views/template/formUpload.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            {{ $game->name }}
-            Go Max
+<div class="container">
+    @if (isset($error))
+        <div class="bg-danger text-white">Les parametres ne sont pas valides</div>
+    @endif
+    <form class="form" action="{{ route('upload_template', ['game' => $gameId]) }}" method="POST">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <label for="name">Nom du template</label>
+            <input type="text" class="form-control" id="name" name="name">
         </div>
-    </div>
+        <div class="form-group">
+            <label for="datas">Template</label>
+            <textarea type="textarea" class="form-control" id="datas" rows="6" name="datas"></textarea>
+        </div>
+        <input type="submit" class="btn btn-primary" value="Créer">
+    </form>
+</div>
 @endsection
